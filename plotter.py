46ad25fcd306bf76eb05db1a5585d5cf10bfc0c0
Elena Galbally <elenagal@stanford.edu>
2018-06-01 15:51:40 -0700

    plot vs epoch. Not iteration

--- plotter.py
+++ plotter.py
@@ -19,12 +19,7 @@ import sys
 #Data to plot
 data_dev = np.genfromtxt("./" + sys.argv[-2])
 data_train = np.genfromtxt("./" + sys.argv[-1]) #sys.argv[-1] looks at last terminal input 
-its_train = data_train[:,1] #iterations
-its_dev = data_dev[:,1] #iterations
-batch_size_train = np.max(its_train) + 1
-batch_size_dev = np.max(its_dev) + 1
-epochs_train = data_train[:,0] + its_train/batch_size_train
-epochs_dev = data_dev[:,0] + its_dev/batch_size_dev
+epochs = data_train[:,0] 
 loss_train = data_train[:,4]
 loss_dev = data_dev[:,3]
 acc_train = data_train[:,5]
@@ -33,24 +28,24 @@ acc_dev = data_dev[:,4]
 #Loss vs. Epoch
 plt.figure(1)
 plt.subplot(1,2,1)
-plt.plot(epochs_train, loss_train,label='train')
-plt.plot(epochs_dev,loss_dev,label='dev')
+plt.plot(epochs,loss_train,label='train')
+plt.plot(epochs,loss_dev,label='dev')
 plt.legend(loc='best')
-plt.xticks(range(1,1+int(max(data_train[:,0]))))
-plt.xlabel('epoch')
-plt.ylabel('loss')
+# plt.xticks(range(1,1+int(max(data_train[:,0]))))
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
 plt.title('Loss vs. Epoch')
 
 #Accuracy vs. Epoch
 plt.subplot(1,2,2)
 plt.title('Accuracy vs. Epoch')
-plt.plot(epochs_train, loss_train,label='train')
-plt.plot(epochs_dev,loss_dev,label='dev')
+plt.plot(epochs, loss_train,label='train')
+plt.plot(epochs,loss_dev,label='dev')
 plt.legend(loc='best')
-plt.xticks(range(1,1+int(max(data_train[:,0]))))
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.title('Loss vs. Epoch')
+# plt.xticks(range(1,1+int(max(data_train[:,0]))))
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Loss vs. Accuracy')
 
 #Show and save
 plt.savefig( sys.argv[-1] + ".png")
